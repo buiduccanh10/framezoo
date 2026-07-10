979b57c743322da25e95dd9770614472094e18ee
buiduccanh10 <canhbd@ycomm.vn>
2026-07-10 13:23:13 +0700

    chore: increment package versions to 6.3.4 for desktop and frontend; add version.json endpoint and related PWA updates

--- betamovie-fe/src/vite-env.d.ts
+++ betamovie-fe/src/vite-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
+
+declare const __APP_BUILD_ID__: string;
+
+interface ImportMetaEnv {
+  readonly PACKAGE_VERSION: string;
+}
