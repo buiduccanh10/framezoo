10c6c1329decb370765a0e2502766b6658a6367e
buiduccanh10 <canhbd@ycomm.vn>
2026-07-20 14:56:36 +0700

    Revert "chore: increase CPU thread count for subtitle alignment service from 2 to 4 in production configurations"
    This reverts commit 161c0b6b656b34375992a111ac47a6fcc754cc61.

--- betamovie-be/server/api/subtitles/align.post.ts
+++ betamovie-be/server/api/subtitles/align.post.ts
@@ -118,7 +118,7 @@ const chooseWindows = (
   const excluded = rawSegments
     .map(normalizeSegment)
     .filter(({ startMs, endMs }) => startMs !== null && endMs !== null && endMs > startMs);
-
+  
   const introEnd = excluded
     .filter(({ type }) => type === 'intro' || type === 'recap')
     .filter(({ startMs }) => startMs !== null && startMs < 300_000)
@@ -141,6 +141,7 @@ const chooseWindows = (
 
   return windows;
 };
+
 
 const parseProxyHeaders = (rawHeaders: string | null) => {
   if (!rawHeaders) return {};
@@ -255,6 +256,7 @@ const fetchAlignmentStream = async (
     }
   }, 15_000);
 
+  // Background stream processor
   (async () => {
     const decoder = new TextDecoder();
     let buffer = '';
@@ -370,7 +372,7 @@ export default defineEventHandler(async event => {
       internalSourceUrl,
       input.subtitleVtt,
       windows,
-      result => {
+      (result) => {
         const response: AlignResponse = {
           ...result,
           windows,
