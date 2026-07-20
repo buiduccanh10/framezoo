c827df87f07f92ed3ac1acc5737a62df43b2a6ca
BuiDucCanh <buiduccanh10@gmail.com>
2026-07-20 23:54:06 +0700

    refactor: simplify fetcher functions and remove obsolete source definitions

--- betamovie-fe/src/backend/providers/fetchers.ts
+++ betamovie-fe/src/backend/providers/fetchers.ts
@@ -1,89 +1,7 @@
 import { sendExtensionRequest } from "@/backend/extension/messaging";
-import { getApiToken, setApiToken } from "@/backend/helpers/providerApi";
-import {
-  Fetcher,
-  makeSimpleProxyFetcher,
-  setM3U8ProxyUrl,
-} from "@/lib/providers";
-import { getM3U8ProxyUrls, getProxyUrls } from "@/utils/proxyUrls";
+import { Fetcher } from "@/lib/providers";
 
 import { convertBodyToObject, getBodyTypeFromBody } from "../extension/request";
-
-function makeLoadbalancedList(getter: () => string[]) {
-  let listIndex = -1;
-  return () => {
-    const fetchers = getter();
-    if (listIndex === -1 || listIndex >= fetchers.length) {
-      listIndex = Math.floor(Math.random() * fetchers.length);
-    }
-    const proxyUrl = fetchers[listIndex];
-    listIndex = (listIndex + 1) % fetchers.length;
-    return proxyUrl;
-  };
-}
-
-export const getLoadbalancedProxyUrl = makeLoadbalancedList(getProxyUrls);
-function getEnabledM3U8ProxyUrls() {
-  const allM3U8ProxyUrls = getM3U8ProxyUrls();
-  const enabledProxies = localStorage.getItem("m3u8-proxy-enabled");
-
-  if (!enabledProxies) {
-    return allM3U8ProxyUrls;
-  }
-
-  try {
-    const enabled = JSON.parse(enabledProxies);
-    return allM3U8ProxyUrls.filter(
-      (_url, index) => enabled[index.toString()] !== false,
-    );
-  } catch {
-    return allM3U8ProxyUrls;
-  }
-}
-
-export const getLoadbalancedM3U8ProxyUrl = makeLoadbalancedList(
-  getEnabledM3U8ProxyUrls,
-);
-
-async function fetchButWithApiTokens(
-  input: RequestInfo | URL,
-  init?: RequestInit | undefined,
-): Promise<Response> {
-  const apiToken = await getApiToken();
-  const headers = new Headers(init?.headers);
-  if (apiToken) headers.set("X-Token", apiToken);
-  const response = await fetch(
-    input,
-    init
-      ? {
-          ...init,
-          credentials: "include",
-          headers,
-        }
-      : { credentials: "include" },
-  );
-  const newApiToken = response.headers.get("X-Token");
-  if (newApiToken) setApiToken(newApiToken);
-  return response;
-}
-
-export function setupM3U8Proxy() {
-  const proxyUrl = getLoadbalancedM3U8ProxyUrl();
-  if (proxyUrl) {
-    setM3U8ProxyUrl(proxyUrl);
-  }
-}
-
-export function makeLoadBalancedSimpleProxyFetcher() {
-  const fetcher: Fetcher = async (a, b) => {
-    const currentFetcher = makeSimpleProxyFetcher(
-      getLoadbalancedProxyUrl(),
-      fetchButWithApiTokens,
-    );
-    return currentFetcher(a, b);
-  };
-  return fetcher;
-}
 
 function makeFinalHeaders(
   readHeaders: string[],
@@ -118,3 +36,10 @@ export function makeExtensionFetcher() {
   };
   return fetcher;
 }
+
+export const getLoadbalancedProxyUrl = () => {
+  return "";
+};
+export const getLoadbalancedM3U8ProxyUrl = () => {
+  return "";
+};
