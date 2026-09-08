0298c264cf3a4b63c0c5043a14dd816fc9c3fc75
buiduccanh10 <canhbd@ycomm.vn>
2026-09-08 16:35:13 +0700

    fix: update background opacity syntax in BlackOverlay component

--- framezoo-desktop/renderer-src/src/components/player/base/BlackOverlay.tsx
+++ framezoo-desktop/renderer-src/src/components/player/base/BlackOverlay.tsx
@@ -5,7 +5,7 @@ export function BlackOverlay(props: { show?: boolean }) {
     <Transition
       animation="fade"
       show={props.show}
-      className="absolute inset-0 w-full h-full bg-black bg-opacity-20 pointer-events-none"
+      className="absolute inset-0 w-full h-full bg-black/20 pointer-events-none"
     />
   );
 }
